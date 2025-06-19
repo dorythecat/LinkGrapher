1e16da6a68c0ec4ba7d3624417ebfb15a35e3d5f
Dory <pemegomachado@gmail.com>
2025-06-19 17:13:43 +0200

    Added vertex coloring and edge weight

--- main.py
+++ main.py
@@ -15,17 +15,18 @@ same_page_links = re.findall('href="/([^"]+?)(?=[?"])', html)  # Find all relati
 for i in range(len(same_page_links)):
     same_page_links[i] = url + "/" + same_page_links[i]  # Convert relative links to absolute links
 
-print("Direct Links:")
-print(direct_links)
-print("\nSame Page Links:")
-print(same_page_links)
-
 g = gt.Graph(directed=True)  # Create a directed graph 
 
+eweight = g.new_ep("float")
+vcolor = g.new_vp("string")
+
 origin = g.add_vertex()  # Add an origin vertex
+vcolor[origin] = "#ff0000"  # Color the origin vertex red
 
-for link in direct_links + same_page_links:
+for link in direct_links:
     v = g.add_vertex()  # Add each link as a vertex in the graph 
-    g.add_edge(origin, v)
+    vcolor[v] = "#0000ff"  # Color the vertex blue
+    e = g.add_edge(origin, v)
+    eweight[e] = 3.0  # Set edge weight to 1.0
 
-gt.graph_draw(g, vertex_text=g.vertex_index, output="output.svg")
+gt.graph_draw(g, vertex_fill_color=vcolor, edge_pen_width=eweight, output="output.svg")
