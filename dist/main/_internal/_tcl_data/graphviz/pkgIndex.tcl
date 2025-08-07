3e2c78849e2ab7b51a280cf1403464f36dcf327f
Dory <pemegomachado@gmail.com>
2025-08-07 21:30:55 +0200

    Updated README

--- dist/main/_internal/_tcl_data/graphviz/pkgIndex.tcl
+++
@@ -1,9 +0,0 @@
-package ifneeded Tcldot 13.1.0 "
-	load [file join $dir libtcldot.so] Tcldot"
-package ifneeded Tclpathplan 13.1.0 "
-	load [file join $dir libtclplan.so] Tclpathplan"
-package ifneeded Gdtclft 13.1.0 "
-	load [file join $dir libgdtclft.so] Gdtclft"
-package ifneeded gv 0 "
-	load [file join $dir libgv_tcl.so] gv"
-# end
